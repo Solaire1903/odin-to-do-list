9f6ca2805401e5f29087124c6a789b3dc79c3f63
Solaire1903 <marvkie@gmail.com>
2026-07-28 19:17:43 +0200

    Add ToDoList class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import "./styles.css";
-import ToDoTask from "./logic.js";
+import ToDoList from "./logic.js";
 
-console.log(new ToDoTask("Test").title);
+const list = new ToDoList("TestList");
+list.createTask("1");
+list.createTask("2");
+list.createTask("3");
+list.createTask("4");
+console.table(list.tasks);
+
+list.removeTask(list.tasks[0].id);
+console.table(list.tasks);
+list.removeTask(list.tasks[0].id);
+console.table(list.tasks);
+list.removeTask(list.tasks[0].id);
+console.table(list.tasks);
+list.removeTask(list.tasks[0].id);
+console.table(list.tasks);
--- src/logic.js
+++ src/logic.js
@@ -3,18 +3,47 @@ class ToDoTask {
     #description;
     #dueDate;
     #priority;
+    #id;
 
     constructor(title, description, dueDate, priority) {
         this.#title = title;
         this.#description = description;
         this.#dueDate = dueDate;
         this.#priority = priority;
+        this.#id = crypto.randomUUID();
     }
 
-    get title() {
-        return this.#title;
+    get id() {
+        return this.#id;
     }
-    
+
 }
 
-export default ToDoTask;
+class ToDoList {
+    #title;
+    #tasks;
+
+    constructor(title) {
+        this.#title = title;
+        this.#tasks = [];
+    }
+
+    get tasks() {
+        return this.#tasks;
+    }
+
+    createTask(title, description, dueDate, priority) {
+        this.#tasks.push(new ToDoTask(title, description, dueDate, priority));
+    }
+
+    removeTask(taskId) {
+        for (let index in this.#tasks) {
+            if (this.#tasks[index].id === taskId) {
+                this.#tasks.splice(index, 1);
+                return;
+            }
+        };
+    }
+}
+
+export default ToDoList;
