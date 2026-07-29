f04630dafcbd5058dfc8b6db8de3efc97a505378
Solaire1903 <marvkie@gmail.com>
2026-07-29 18:09:59 +0200

    Add renderTasks function

--- src/controller.js
+++ src/controller.js
@@ -5,7 +5,13 @@ function loadApp() {
     listContainer.createList("List 1");
     listContainer.createList("List 2");
     listContainer.createList("List 3");
+
+    listContainer.lists[0].createTask("T1");
+    listContainer.lists[0].createTask("T2");
+    listContainer.lists[0].createTask("T3");
+
     view.renderLists(listContainer.lists);
+    view.renderTasks(listContainer.lists[0].tasks);
 }
 
 export default loadApp;
--- src/view.js
+++ src/view.js
@@ -1,11 +1,19 @@
 class View {
-    renderLists(lists) {
-        const container = document.querySelector(".container");
+    #container = document.querySelector(".container");
 
+    renderLists(lists) {
         for (const list of lists) {
             const listElement = document.createElement("p");
             listElement.textContent = list.title;
-            container.appendChild(listElement);
+            this.#container.appendChild(listElement);
+        }
+    }
+
+    renderTasks(tasks) {
+        for (const task of tasks) {
+            const taskElement = document.createElement("p");
+            taskElement.textContent = task.title;
+            this.#container.appendChild(taskElement);
         }
     }
 }
