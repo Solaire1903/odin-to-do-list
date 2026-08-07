c0d46deb74ad61e9d971953d015cc7cfcaadc467
Solaire1903 <marvkie@gmail.com>
2026-08-07 19:38:37 +0200

    Allow dynamic rendering of project cards

--- src/controller.js
+++ src/controller.js
@@ -2,13 +2,13 @@ import projectContainer from "./logic.js";
 import view from "./view.js";
 
 function loadApp() {
-    projectContainer.createProject("Project 4");
-    projectContainer.createProject("Project 5");
-    projectContainer.createProject("Project 6");
+    projectContainer.createProject("Project 1");
+    projectContainer.createProject("Project 2");
+    projectContainer.createProject("Project 3");
 
-    projectContainer.projects[0].createTask("Task 4");
-    projectContainer.projects[0].createTask("Task 5");
     projectContainer.projects[0].createTask("Task 6");
+    projectContainer.projects[0].createTask("Task 7");
+    projectContainer.projects[0].createTask("Task 8");
 
     view.renderProjects(projectContainer.projects);
     view.renderTasks(projectContainer.projects[0].tasks);
--- src/view.js
+++ src/view.js
@@ -1,12 +1,46 @@
+import pencilSVG from "./img/pencil.svg"
+import trashCanSVG from "./img/trash-can.svg"
+
 class View {
     renderProjects(projects) {
         const projectList = document.getElementById("project-list")
 
         for (const project of projects) {
-            const projectElement = document.createElement("li");
-            projectElement.classList.add("project");
-            projectElement.textContent = project.title;
-            projectList.appendChild(projectElement);
+            const listElement = document.createElement("li");
+
+            const projectCard = document.createElement("div");
+            projectCard.classList.add("project-card");
+
+            const projectTitle = document.createElement("p");
+            projectTitle.textContent = project.title;
+
+            const buttonContainer = document.createElement("div");
+
+            const editButton = document.createElement("button");
+
+            const deleteButton = document.createElement("button");
+
+            const deleteButtonImage = document.createElement("img");
+            deleteButtonImage.src = trashCanSVG;
+            deleteButtonImage.alt = "Delete";
+
+            const editButtonImage = document.createElement("img");
+            editButtonImage.src = pencilSVG;
+            editButtonImage.alt = "Edit";
+
+            projectList.appendChild(listElement);
+
+            listElement.appendChild(projectCard);
+
+            projectCard.appendChild(projectTitle);
+            projectCard.appendChild(buttonContainer);
+
+            buttonContainer.appendChild(editButton);
+            buttonContainer.appendChild(deleteButton);
+
+            deleteButton.appendChild(deleteButtonImage);
+
+            editButton.appendChild(editButtonImage);
         }
     }
 
