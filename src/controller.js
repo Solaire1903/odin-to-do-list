e22e70b164658f93188a198028628fdce0fddb28
Solaire1903 <marvkie@gmail.com>
2026-08-20 16:39:43 +0200

    Update function name

--- src/controller.js
+++ src/controller.js
@@ -51,7 +51,7 @@ function handleDeleteProject(projectId) {
         projectContainer.activeProject = null;
         view.renderActiveProjectTitle("");
         view.renderTasks([]);
-        view.removeTaskButton();  
+        view.hideTaskButton();  
     }
 
     view.applyProjectEventListeners(handleActiveProject);
