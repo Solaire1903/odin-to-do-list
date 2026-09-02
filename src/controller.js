d42a7e40645002843178818cdcee6102855c218c
Solaire1903 <marvkie@gmail.com>
2026-09-02 17:50:31 +0200

    Add function for checking storage availability

--- src/controller.js
+++ src/controller.js
@@ -119,6 +119,29 @@ function handleDeleteTask(taskId) {
 }
 
 /**
+ * Checks, if the given storage type is available to use
+ * @param {string} type The storage type
+ * @returns True, if storage is available
+ */
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+            e instanceof DOMException &&
+            e.name === "QuotaExceededError" &&
+            storage &&
+            storage.length !== 0
+        );
+    }
+}
+
+/**
  * Loads the app on initial startup
  */
 function loadApp() {
